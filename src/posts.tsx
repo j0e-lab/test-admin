43e6f6681fe43b69fbf097c5784db2927ca4ddf9
Gatekeeper-joe <investorjk5x@gmail.com>
2024-02-27 18:23:34 +0900

    Adding Search And Filters To The List

--- src/posts.tsx
+++ src/posts.tsx
@@ -1,8 +1,14 @@
-import { List, Datagrid, TextField, ReferenceField, EditButton, Edit, SimpleForm, ReferenceInput, TextInput, Create } from "react-admin";
+import { List, Datagrid, TextField, ReferenceField, EditButton, Edit, SimpleForm, ReferenceInput, TextInput, Create, useRecordContext } from "react-admin";
+
+// コンポーネントのリストを作る場合、keyプロパティをつけている必要がある（つけてないとtypescriptに怒られる）
+const postFilters = [
+  <TextInput key="search" source="q" label="Search" alwaysOn />, // 入力したワードとマッチするレコードを検索する
+  <ReferenceInput key="user" source="userId" label="User" reference="users" />, // プルダウンからフィルターして表示したいレコードを選べる
+];
 
 export const PostList = () => (
-  <List>
-    <Datagrid>
+  <List filters={postFilters}>
+    <Datagrid rowClick="show">
       <TextField source="id" />
       <ReferenceField source="userId" reference="users" link="show" />
       <TextField source="title" />
@@ -11,8 +17,14 @@ export const PostList = () => (
   </List>
 );
 
+const PostTitle = () => {
+  const record = useRecordContext(); // 指定された列のレコード情報を取得
+  // recordがない状態でレンダリングされる場合があるので常にrecordの有無をチェックする処理が必要
+  return <span>Post {record ? `"${record.title}"` : ''}</span>;
+};
+
 export const PostEdit = () => (
-  <Edit>
+  <Edit title={<PostTitle />}>
     <SimpleForm>
       {/* InputPropsで入力値に関する設定ができる。下記の場合入力不可にできる */}
       <TextInput source="id" InputProps={{ disabled: true }} />
