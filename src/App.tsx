e7af53f8e41cbd10ad165c41a52342e300200527
Gatekeeper-joe <investorjk5x@gmail.com>
2024-02-27 17:46:32 +0900

    Adding Creation Capabilities

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Admin, Resource, ShowGuesser } from "react-admin";
 import { dataProvider } from "./dataProvider";
-import { PostList, PostEdit } from "./posts";
+import { PostList, PostEdit, PostCreate } from "./posts";
 import { UserList } from "./users";
 
 export const App = () => (
@@ -8,7 +8,12 @@ export const App = () => (
   // Adminコンポーネントは最低1つのResourceコンポーネントを含んでいる必要がある
   // list propsはname propsで指定したテーブルのレコードを取得するようadminに通知している
   <Admin dataProvider={dataProvider}>
-    <Resource name="posts" list={PostList} edit={PostEdit} />
+    <Resource
+      name="posts"
+      list={PostList}
+      edit={PostEdit}
+      create={PostCreate}
+    />
     <Resource
       name="users"
       list={UserList}
