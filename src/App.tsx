d39d3a12826415ad4262b7d135ee8da48eca4cad
Gatekeeper-joe <investorjk5x@gmail.com>
2024-02-27 12:31:57 +0900

    Mapping API Endpoints With Resources

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,16 @@ import {
   Admin,
   Resource,
   ListGuesser,
-  EditGuesser,
-  ShowGuesser,
 } from "react-admin";
 import { dataProvider } from "./dataProvider";
 
-export const App = () => <Admin dataProvider={dataProvider}></Admin>;
+export const App = () => (
+  // dataProviderとはreact-admin（以下admin）がapiと通信する際のアダプターとして機能するもの
+  // Adminコンポーネントは最低1つのResourceコンポーネントを含んでいる必要がある
+  <Admin dataProvider={dataProvider}>
+    <Resource
+      name="users"
+      list={ListGuesser} // usersレコードを取得するようadminに通知
+    />
+  </Admin>
+);
